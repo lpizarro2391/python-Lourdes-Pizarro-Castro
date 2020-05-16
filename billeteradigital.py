ac69051bddda47aab88ddf4fc82d2d9b54ff2d4f
lpizarro <lpizarro2391@github.com>
2020-05-15 19:04:14 -0600

    Resumen de opcion 3

--- billeteradigital.py
+++ billeteradigital.py
@@ -3,3 +3,10 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 from datetime import datetime
+
+def resMoned(nombremone,CantMone,preciomon):
+    print(nombremone.upper())
+    print("En este momento usted possee: ",CantMone[nombremone],nombremone.upper())
+    print("Equivalente a: ,",str(CantMone[nombremone]*preciomon[nombremone]),"USD")
+
+    
