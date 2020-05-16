4f025b2484aadcbfd23039ae54d51de93b7dd32f
lpizarro <lpizarro2391@github.com>
2020-05-15 22:33:41 -0600

    acomodo indentacion

--- billeteradigital.py
+++ billeteradigital.py
@@ -46,73 +46,73 @@ def monedasDict():
     print("precio:",moneda_dict)
     return moneda_dict
 
-    def principal():
-        cond = "1"
-        micode = random.randint(100,200)
-        historial = [['Fecha(D/M/A)', 'Moneda','Tipo Op','Origen','Destino','Monto(USD)']]
-        precio = monedasDict()
-        defmoneda = {}
+def principal():
+    cond = "1"
+    micode = random.randint(100,200)
+    historial = [['Fecha(D/M/A)', 'Moneda','Tipo Op','Origen','Destino','Monto(USD)']]
+    precio = monedasDict()
+    defmoneda = {}
 
-        while cond in ['1','2','3','4','5','6']:
-            print("*********************  MENÚ PRINCIPAL  *************************")
-            print("     #     Ha ingresado con el código: ",
+    while cond in ['1','2','3','4','5','6']:
+        print("*********************  MENÚ PRINCIPAL  *************************")
+        print("     #     Ha ingresado con el código: ",
                   micode, "                  #")
-            print("     #     Bienvenido a coinmarketcap. Digite una opción       #")
-            print("     #     1: Recibir cantidad.                                #")
-            print("     #     2: Transaferir monto.                               #")
-            print("     #     3: Mostrar balance de una moneda.                   #")
-            print("     #     4: Mostrar balance general.                         #")
-            print("     #     5: Mostrar histórico de transacciones.              #")
-            print("     #     6: Salir del programa.                              #")
-            print("     ***********************************************************")
-            print("Notas:")
-            print("- Las transferencias se realizan en USD")
-            print("- Escriba la abreviatura de la moneda")
-            print("------------------------------------------------------------------------------------")
-            cond = input("").strip() 
-            fecha = str(datetime.now().day)+' / '+ \
+        print("     #     Bienvenido a coinmarketcap. Digite una opción       #")
+        print("     #     1: Recibir cantidad.                                #")
+        print("     #     2: Transaferir monto.                               #")
+        print("     #     3: Mostrar balance de una moneda.                   #")
+        print("     #     4: Mostrar balance general.                         #")
+        print("     #     5: Mostrar histórico de transacciones.              #")
+        print("     #     6: Salir del programa.                              #")
+        print("     ***********************************************************")
+        print("Notas:")
+        print("- Las transferencias se realizan en USD")
+        print("- Escriba la abreviatura de la moneda")
+        print("------------------------------------------------------------------------------------")
+        cond = input("").strip() 
+        fecha = str(datetime.now().day)+' / '+ \
                 str(datetime.now().month)+' / '+str(datetime.now().year)
-            h = [0, 0, 0, 0, 0, 0,]
-            if cond =='1':
-                print("Recibir Cantidad")
-                codeusertransfiere = random.randint(300,400)
-                cripto = input("Indique la moneda que va a recibir (BTC,ETH,XRP,BCH,LTC,EOS,BNB,XTZ)").upper().strip()
-                if cripto in precio:
-                    if codeusertransfiere != micode:
-                        cant = int(input("Que cantidad va a recibir?"))
-                        if cripto in defmoneda:
-                            defmoneda[cripto] += cant
-                        else:
-                            defmoneda[cripto] = cant
-                        h = [fecha, cripto.upper(),'Recibir',str(codeusertransfiere),str(micode),str(cant*precio[cripto])]
-                        historial.append(h)
-                        print("Transferencia exitosa de", cant, cripto)
-                        seguir = input("Desea volver al menú principal?(S/N):").upper()
-                        if (seguir =="S"):
-                            print()
-                        elif (seguir =="N"):
-                            exit()
-                    else: 
-                        print("ERROR: El codigo de Origen y Destino no puede ser el mismo.")
-                else:
-                    print("Error: La criptomoneda ", cripto, "no está dentro de las opciones")
+        h = [0, 0, 0, 0, 0, 0,]
+        if cond =='1':
+            print("Recibir Cantidad")
+            codeusertransfiere = random.randint(300,400)
+            cripto = input("Indique la moneda que va a recibir (BTC,ETH,XRP,BCH,LTC,EOS,BNB,XTZ)").upper().strip()
+            if cripto in precio:
+                if codeusertransfiere != micode:
+                    cant = int(input("Que cantidad va a recibir?"))
+                    if cripto in defmoneda:
+                        defmoneda[cripto] += cant
+                    else:
+                        defmoneda[cripto] = cant
+                    h = [fecha, cripto.upper(),'Recibir',str(codeusertransfiere),str(micode),str(cant*precio[cripto])]
+                    historial.append(h)
+                    print("Transferencia exitosa de", cant, cripto)
                     seguir = input("Desea volver al menú principal?(S/N):").upper()
-                    if (seguir == "S"):
+                    if (seguir =="S"):
                         print()
                     elif (seguir =="N"):
                         exit()
-            elif cond =='2':
-                print("Transferir Monto")
-                validadestinat = False
-                while not validadestinat:
-                    codigodestinatario = input("Ingrese el codigo del destinatario: ")
-                    validadestinat = is_int(codigodestinatario)
-                    if (validadestinat and not(300 <= int(codigodestinatario) <= 400)):
-                        validadestinat = False
-                        print("Error: el codigo debe estar entre 300 y 400")
-                    if (validadestinat and (int(codigodestinatario) ==micode)):
-                        validadestinat = False
-                        print("Error: El codigo del remitente y del destinatario no puede ser el mismo")
+                else: 
+                        print("ERROR: El codigo de Origen y Destino no puede ser el mismo.")
+            else:
+                print("Error: La criptomoneda ", cripto, "no está dentro de las opciones")
+                seguir = input("Desea volver al menú principal?(S/N):").upper()
+                if (seguir == "S"):
+                    print()
+                elif (seguir =="N"):
+                    exit()
+        elif cond =='2':
+            print("Transferir Monto")
+            validadestinat = False
+            while not validadestinat:
+                codigodestinatario = input("Ingrese el codigo del destinatario: ")
+                validadestinat = is_int(codigodestinatario)
+                if (validadestinat and not(300 <= int(codigodestinatario) <= 400)):
+                    validadestinat = False
+                    print("Error: el codigo debe estar entre 300 y 400")
+                if (validadestinat and (int(codigodestinatario) ==micode)):
+                    validadestinat = False
+                    print("Error: El codigo del remitente y del destinatario no puede ser el mismo")
                 cripto = input ("Qué Criptomoneda va a enviar? (BTC, ETH, XRP, BCH, LTC, EOS, BNB, XTZ) ").upper().strip()
                 if cripto in defmoneda:
                     cant = int(input("Que cantidad va a enviar?"))
@@ -141,50 +141,50 @@ def monedasDict():
                         print()
                     elif ( seguir =="N"):
                         exit()
-            elif cond == '3':
-                print("Mostrar balance de una moneda")
-                mon = input("Indique qué moneda va a consultar? (BTC, ETH, XRP, BCH, LTC, EOS, BNB, XTZ) ").upper().strip()
-                if mon in defmoneda:
-                    resMoned(mon,defmoneda,precio)
-                    seguir = input("Desea volver al menu principal? (S/N):").upper()
-                    if (seguir =="S"):
-                        print()
-                    elif(seguir =="N"):
-                        exit()
-                else: 
-                    print("Error: Usted no posee" ,mon,"Suficientes.")
-                    seguir = input("Volver al menú principal? (S/N):").upper()
-                    if(seguir =="S"):
-                        print()
-                    elif (seguir =="N"):
-                        exit()
-            elif cond =='4':
-                print("Mostrar balance general")
-                lisC = list(defmoneda.keys())
-                for i in lisC:
-                    resMoned(i,defmoneda,precio)
-                seguir = input("Desea Volver al menu principal? (S/N):").upper()
+        elif cond == '3':
+            print("Mostrar balance de una moneda")
+            mon = input("Indique qué moneda va a consultar? (BTC, ETH, XRP, BCH, LTC, EOS, BNB, XTZ) ").upper().strip()
+            if mon in defmoneda:
+                resMoned(mon,defmoneda,precio)
+                seguir = input("Desea volver al menu principal? (S/N):").upper()
                 if (seguir =="S"):
                     print()
                 elif(seguir =="N"):
                     exit()
-            elif cond =='5':
-                print("Mostrar historico de transacciones")
-                printMat(historial)
-                seguir = input("Desea volver al menu principal? (S/N):").upper()
-                if (seguir =="S"):
+            else: 
+                print("Error: Usted no posee" ,mon,"Suficientes.")
+                seguir = input("Volver al menú principal? (S/N):").upper()
+                if(seguir =="S"):
                     print()
                 elif (seguir =="N"):
                     exit()
-            elif cond=='6':
-                cond = '7'
-                print("hasta la próxima!")
-            else:
-                cond ='1'
-                print("Opcion inválida, por favor intente nuevamente")
+        elif cond =='4':
+            print("Mostrar balance general")
+            lisC = list(defmoneda.keys())
+            for i in lisC:
+                resMoned(i,defmoneda,precio)
+            seguir = input("Desea Volver al menu principal? (S/N):").upper()
+            if (seguir =="S"):
+                print()
+            elif(seguir =="N"):
+                exit()
+        elif cond =='5':
+            print("Mostrar historico de transacciones")
+            printMat(historial)
+            seguir = input("Desea volver al menu principal? (S/N):").upper()
+            if (seguir =="S"):
+                print()
+            elif (seguir =="N"):
+                exit()
+        elif cond=='6':
+            cond = '7'
+            print("hasta la próxima!")
+        else:
+            cond ='1'
+            print("Opcion inválida, por favor intente nuevamente")
     
     
-    principal()
+principal()
 
 
 
